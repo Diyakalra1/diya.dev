2c05f9ed4b30e6335957e8615987df70e304ed35
Diyakalra1 <158696444+Diyakalra1@users.noreply.github.com>
2026-08-12 21:30:58 +0530

    icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Diya Kalra",
   description: "Portfolio showcasing AI systems, software engineering projects, competitive programming, and full-stack development.",
+  icons: {
+    icon: "/icon.png",
+  }
 };
 
 export default function RootLayout({
